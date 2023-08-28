7bb16ec6d5a9994f908eccf1c6f270783e179057
ebert-escalant <ebertescalant@gmail.com>
2023-08-28 16:21:59 -0500

    fix error in tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,15 @@
 import type { Config } from 'tailwindcss'
-import { nextui } from "@nextui-org/react"
 
 const config: Config = {
 	content: [
 		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
-		'./node_modules/@nextui-org/theme/dist/components/button.js'
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
 	],
 	theme: {
 		extend: {}
 	},
-	plugins: [nextui()]
+	plugins: []
 }
 
 export default config
